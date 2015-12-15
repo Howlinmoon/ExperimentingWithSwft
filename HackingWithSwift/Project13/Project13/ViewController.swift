23d54fcfcc80f3127750a267d3dc41d7c4460b80
Jim Veneskey <github@strifecrafter.com>
2015-12-15 05:56:08 -0500

    Created the initial UI - ready for coding

---
+++ HackingWithSwift/Project13/Project13/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by jim Veneskey on 12/15/15.
+//  Copyright © 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBOutlet weak var intensity: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func changeFilter(sender: AnyObject) {
+    
+    }
+
+    @IBAction func save(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func intensityChanged(sender: AnyObject) {
+    
+    }
+    
+}
+
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Starting the Tutorial Series - Hacking With Swift
 
 Continue here:
-https://www.hackingwithswift.com/read/13/1/setting-up
+https://www.hackingwithswift.com/read/13/3/importing-a-picture-uiimage
 
 Starting Project 13
 
